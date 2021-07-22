ac176277787f72ceefe4b4009451e42bf9d28fe3
sathishsangee <geethadeepa1.gmail.com>
2021-07-22 12:46:32 +0100

    Pipeline-new

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,22 @@
-name: Pages Pipeline
+#!/bin/bash
+set -e
 
-on:
-  push:
-    branches: [master]
+echo Namespace = "$1"
+echo Releasename = "$2"
+echo "----------------------Installing pages----------------------------------"
 
-jobs:
-  deploy-to-staging:
-    runs-on: ubuntu-latest
-    steps:
-      - uses: actions/checkout@v2
-      - name: AWS Credentials
-        uses: aws-actions/configure-aws-credentials@v1
-        with:
-          aws-access-key-id: ${{ secrets.AWS_ACCESS_KEY_ID }}
-          aws-secret-access-key: ${{ secrets.AWS_SECRET_ACCESS_KEY }}
-          aws-region: ${{ secrets.AWS_REGION }}
-      - name: Configure EKS
-        run: |
-          aws eks --region ap-south-1 update-kubeconfig --name dees-cloud
 
-      - name: Install Kubectl
-        uses: actions/checkout@v2
-      - name: Check kubectl
-        run: |
-          ls
-          bash ./scripts/install-kubectl.sh
-      - name: Install Helm3
-        run: |
-          bash ./scripts/install-helm.sh
-      - name: Deploy to staging
-        run: |
-          bash ./scripts/deploy.sh ${{ secrets.STAGING_NAMESPACE }} ${{ secrets.RELEASE_NAME }}
+NAMESPACE="$1"
+RELEASE_NAME="$2"
+
+kubectl get ns "$NAMESPACE" &> /dev/null || kubectl create ns "$NAMESPACE"
+
+helm upgrade --install "$RELEASE_NAME" pages -n "$NAMESPACE" --debug
+helm list
+kubectl config current-context
+sleep 30s
+
+kubectl get pods -n "$NAMESPACE"
+
+
+echo "----------------------Completed Installation of  pages----------------------------------"
